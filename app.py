38df2c46f3f5f03bc7573b23a133d6e28f4df84f
Kevin2211 <36496209+Kevin2211@users.noreply.github.com>
2021-08-28 08:17:42 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+
+import streamlit as st
+from predict_page import show_predict_page
+from dashboard_page import show_dashboard_page
+
+
+page = st.sidebar.selectbox("Dashboard or Predict", ("Dashboard", "Predict"))
+
+if page == "Predict":
+    show_predict_page()
+else:
+    show_dashboard_page()
+
+
+
+
+
